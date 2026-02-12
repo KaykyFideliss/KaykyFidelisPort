e43af273de41eff112c09cf70cae5191c008de7d
Kayky Fidelis <kayky.fid@gmail.com>
2026-02-12 12:16:27 -0300

    acabando section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,62 +13,86 @@ export default function HorizontalSection() {
   const screen2Ref = useRef<HTMLDivElement>(null);
 const bigTextRef = useRef<HTMLHeadingElement>(null);
 const imagesRef = useRef<HTMLImageElement[]>([]);
+const windElements = gsap.utils.toArray<HTMLElement>(
+  ".wind-effect"
+);
 
 useLayoutEffect(() => {
   const section = sectionRef.current;
   const wrapper = wrapperRef.current;
   const svg = svgRef.current;
+  
 
   if (!section || !wrapper || !svg) return;
 
   const ctx = gsap.context(() => {
     const scrollLength = wrapper.scrollWidth - window.innerWidth;
 
-    
-    // 🔥 SCROLL HORIZONTAL
-    const horizontalTween = gsap.to(wrapper, {
-      x: -scrollLength,
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        start: "top top",
-        end: () => `+=${scrollLength}`,
-        scrub: true,
-        pin: true,
-        invalidateOnRefresh: true,
-      },
-    });
+    // HORIZONTAL SCROLL
+const horizontalTween = gsap.to(wrapper, {
+  x: -scrollLength,
+  ease: "none",
+  scrollTrigger: {
+    trigger: section,
+    start: "top top",
+    end: () => `+=${scrollLength}`,
+    scrub: true,
+    pin: true,
+    invalidateOnRefresh: true,
+  },
+});
 
-    // 🔥 DESENHAR LINHA
-const path = svg.querySelector("#mainPath") as SVGPathElement;
 
-if (path) {
-  const length = path.getTotalLength();
+    // PATH
+    const path = svg.querySelector("#mainPath") as SVGPathElement;
+    if (!path) return;
 
-  gsap.set(path, {
-    strokeDasharray: length,
-     strokeDashoffset: 0,
-  });
+    const length = path.getTotalLength();
 
-gsap.fromTo(
-  path,
-  { strokeDashoffset: -length },
-  {
-    strokeDashoffset: 0,
-    ease: "none",
-    scrollTrigger: {
-      trigger: section,
-      start: "top top",
-      end: () => `+=${scrollLength}`,
-      scrub: true,
+
+gsap.set(path, {
+  strokeDasharray: length,
+  strokeDashoffset: -length,
+});
+
+gsap.to(path, {
+  strokeDashoffset: 0,
+  ease: "none",
+  scrollTrigger: {
+    trigger: section,
+    start: "top top",
+    end: () => `+=${scrollLength}`,
+    scrub: true,
+  },
+});
+
+windElements.forEach((el, i) => {
+  const distance = 50 + i * 30;
+  gsap.fromTo(
+    el,
+    {
+      x: 0,
+      skewX: 0,
+      opacity: 1,
+       filter: "blur(0px)",
     },
-  }
-);
-}
+    {
+      x: 0,
+      skewX: 0,
+      opacity: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        containerAnimation: horizontalTween,
+        trigger: el,
+        start: "left 80%",
+        end: "left 40%",
+        scrub: true,
+      },
+    }
+  );
+});
 
   }, section);
-
-  
 
   return () => ctx.revert();
 }, []);
@@ -80,7 +104,7 @@ return (
         ref={sectionRef}
         className="relative w-full h-screen overflow-hidden"
       >
-        <div ref={wrapperRef} className="flex h-full">
+        <div ref={wrapperRef} className="flex h-full relative" >
           {/*  tela 1 */}
           <div className="w-[200vw] h-screen shrink-0 flex bg-white ">
             <div className="relative w-full h-screen shrink-0 flex bg-white">
@@ -93,35 +117,26 @@ return (
 
               {/* Segundo título no final dos 200vw */}
               <div className="absolute left-[100vw] top-40 md:top-20 md:top-42 md:px-10 lg:top-16 flex lg:w-1/2 z-20">
-                <h1 className="font-syne text-terciaria text-4xl md:text-5xl lg:text-7xl font-extrabold">
+                <h1 className=" wind-effect font-syne text-terciaria text-4xl md:text-5xl lg:text-7xl font-extrabold">
                   Conhecendo novas tecnologias
                 </h1>
               </div>
             </div>
-<div className="
-  absolute
-  
-  -translate-y-1/2
-  left-50
-  w-full
-  md:w-[550vw]
-  h-[200px]
-  md:h-auto
-  z-10
-  pointer-events-none
-">
-  <svg
-    ref={svgRef}
-    viewBox="0 0 6065 1075"
-    preserveAspectRatio="xMidYMid meet"
-    className="w-full h-full overflow-visible"
-    fill="none"
-  >
+ <div className="absolute top-96 md:top-0 -translate-y-1/2 left-20   w-[550vw] pointer-events-none z-10">
+
+<svg
+  ref={svgRef}
+  viewBox="0 0 6065 1075"
+ 
+  className="h-auto block w-full z-10"
+  fill="none"
+>
     <path
       id="mainPath"
       d="M5914.04 150.034C5914.04 150.034 5589.49 821.575 5258.04 858.034C4858.04 902.034 4866.04 534.034 4398.04 534.034C3700.89 534.034 3894.04 878.034 3210.04 922.034C2627 959.54 2585.96 558.688 2002.04 578.034C1497.29 594.757 1488.54 705.753 998.04 826.034C507.542 946.316 150.04 806.034 150.04 806.034"
       stroke="#0088E8"
-      strokeWidth="120"
+
+      strokeWidth="100"
       strokeLinecap="round"
       filter="url(#glow)"
     />
@@ -129,8 +144,8 @@ return (
 </div>
 
             {/* texto descrição */}
-            <div className="absolute bottom-0 right-0 z-20 w-full md:w-[40rem] p-10 flex justify-end">
-              <p className="text-justify font-syne font-bold  text-base md:text-lg lg:text-xl p-3  text-terciaria ">
+            <div className="absolute bottom-0 right-0 z-20 w-full md:w-[40rem] lg:w-[60rem] p-10 flex justify-end">
+              <p className=" wind-effect text-justify font-syne font-bold  text-base md:text-lg lg:text-3xl p-3   text-terciaria ">
                 {" "}
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos,
                 consequatur? Laudantium rem ad quos at, quasi enim saepe rerum
@@ -147,10 +162,10 @@ return (
             className="relative min-w-[250vw] h-screen shrink-0 bg-white"
           >
             <div className="absolute bottom-0 left-20 p-10 w-80 text-left">
-              <h2 className="text-xl font-syne font-extrabold text-primaria pb-5">
+              <h2 className="text-xl font-syne wind-effect font-extrabold text-primaria pb-5">
                 PROJETOS REALIZADOS
               </h2>
-              <p className="font-syne z-20">
+              <p className="font-syne z-20 wind-effect ">
                 Unique corporate websites
                 Smart open source CMS.
                 E-commerce solutions
@@ -193,15 +208,11 @@ return (
 ].map((item, i) => (
   <div
     key={i}
-    className={`absolute z-20  ${item.position === "top" ? "top-24" : "bottom-24"} w-80`}
+    className={`absolute z-20 wind-effect  ${item.position === "top" ? "top-24" : "bottom-24"} w-80`}
     style={{ left: item.left }}
   >
     <div
-      ref={(el) => {
-        if (el && !imagesRef.current.includes(el)) {
-          imagesRef.current.push(el);
-        }
-      }}
+  
       className="p-8 will-change-transform"
     >
       <h2 className="text-xl font-syne text-start font-extrabold text-primaria pb-5 ">
@@ -210,6 +221,7 @@ return (
       <p className="font-syne text-start ">
         {item.text}
       </p>
+  
     </div>
   </div>
 ))}
@@ -217,7 +229,7 @@ return (
         <div className="absolute inset-0 flex items-center justify-center">
   <h1
     ref={bigTextRef}
-    className="text-4xl lg:text-9xl font-syne font-extrabold text-terciaria z-20"
+    className="text-4xl lg:text-9xl wind-effect font-syne font-extrabold text-terciaria z-20 whitespace-nowrap"
   >
     Sempre utilizando designs modernos
   </h1>
@@ -225,13 +237,15 @@ return (
           </div>
 
 {/* TELA 3 */}
-<div className="min-w-[100vw] h-screen shrink-0 bg-white flex items-center justify-center px-6">
+<div className="w-[120vw] h-screen shrink-0 bg-white flex items-center justify-center px-6">
 
-  <div className="max-w-4xl w-full text-center flex flex-col items-center gap-6">
+  <div className="text-center flex flex-col items-center gap-6 text-terciaria">
 
-    <h1 className="text-xl  z-20 md:text-7xl font-syne font-extrabold ">
-      TUDO QUE VOCÊ PRECISA !!!
-    </h1>
+    
+
+      <h1 className="text-xl  z-20 md:text-6xl font-syne font-extrabold  ">TUDO</h1>
+    <h1  className="text-xl  z-20 md:text-6xl font-syne font-extrabold  ">QUE VOCÊ</h1>
+    <h1  className="text-xl  z-20 md:text-6xl font-syne font-extrabold  ">PRECISA</h1>
   </div>
 
 </div>
